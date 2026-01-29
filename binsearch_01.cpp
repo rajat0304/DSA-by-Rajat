56ac4eb4a327c3be6a8e79846e75d8a2a422c1ba
rajat0304 <rajattripathi0304@gmail.com>
2026-01-29 13:29:48 +0000

    learned to impliment the binearyseacrh in unsorted data given in the array

---
+++ binsearch_01.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int binsearch(const vector<int>&arr,int target){
+    int low=0;
+    int high=arr.size()-1;
+    
+    while(low<high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+            high=mid-1;
+        }
+        else{
+          low=mid+1;  
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<int>score={13,14,15,1,2,3,4,5,6,7,8,9,10,11,12};
+    int target=14;
+    sort(score.begin(),score.end());
+    cout<<"sorted array ";
+    for(int i :score){
+        cout<<i<<" ";
+        }
+        cout<<endl;
+    int result=binsearch(score,target);
+    if(result!=-1){
+        cout<<"element found at the index "<<result<<endl;
+    }
+    else 
+    cout<<"element not found";
+}
