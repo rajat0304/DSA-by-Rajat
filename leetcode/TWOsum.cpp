000c2a6f34007581ab1a342c2363c0336595e015
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-03-18 17:33:21 +0530

    Update TWOsum.cpp

--- leetcode/TWOsum.cpp
+++ leetcode/TWOsum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
+        //using two nested for loops
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
