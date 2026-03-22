8805c445c9dd3f31936b98787564e549001bd91a
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-03-22 22:15:16 +0530

    Create min_rem_achieveTarget_3877.cpp

---
+++ leetcode/min_rem_achieveTarget_3877.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minRemovals(vector<int>& nums, int target) {
+        unordered_map<int,int> dp;
+        dp[0] = 0;
+        
+        for(int a : nums){
+            target ^= a;
+            vector<pair<int, int>> items(dp.begin(), dp.end());
+            
+            for(auto& item : items){
+                int v = item.first, c = item.second;
+                int next_v = v ^ a;
+                int current_c = dp.count(next_v) ? dp[next_v] : 50; 
+                
+                if(c + 1 < current_c){
+                    dp[next_v] = c + 1;
+                }
+            }
+        }
+        
+        return dp.count(target) ? dp[target]: -1; 
+    }
+};
