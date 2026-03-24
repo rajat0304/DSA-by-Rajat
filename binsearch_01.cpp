7c9ecc2eb52bcecbb9ab35f991621194a5e77d15
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-03-24 22:39:29 +0530

    Update binsearch_01.cpp

--- binsearch_01.cpp
+++ binsearch_01.cpp
@@ -21,15 +21,18 @@ int binsearch(const vector<int>&arr,int target){
     return -1;
 }
 int main(){
+    // taking the example of the array(unsorted)
     vector<int>score={13,14,15,1,2,3,4,5,6,7,8,9,10,11,12};
     int target=14;
+    // using STL method to sort the array
     sort(score.begin(),score.end());
-    cout<<"sorted array ";
+    cout<<"sorted array ";// printing the score array after sorting it 
     for(int i :score){
         cout<<i<<" ";
         }
         cout<<endl;
     int result=binsearch(score,target);
+    // pritning the result of the program
     if(result!=-1){
         cout<<"element found at the index "<<result<<endl;
     }
