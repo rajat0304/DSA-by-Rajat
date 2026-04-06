03d60becad1f1385e4e3d956d3cb949264d3f846
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-04-06 22:25:08 +0530

    Update 35.cpp

--- leetcode/35.cpp
+++ leetcode/35.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size();
+        // can use other method as well 
         for(int i=0;i<nums.size();i++){
             if(nums[i]>=target){
                 return i;
