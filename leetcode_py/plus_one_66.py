9bd5af0f5320b9efd736b40ad8f00a335b4560fb
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-04-10 23:34:53 +0530

    Update plus_one_66.py

--- leetcode_py/plus_one_66.py
+++ leetcode_py/plus_one_66.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+              
+            digits[i] = 0
+            
+            if i == 0:
+                digits.insert(0, 1)
+                return digits
+        
+        return digits
