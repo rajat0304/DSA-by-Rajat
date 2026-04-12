19b06df789bed827fd4039150bfd0e7a4a32ee3a
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-04-12 18:57:48 +0530

    Update besttime_to_buyandsell_121.py

--- leetcode_py/besttime_to_buyandsell_121.py
+++ leetcode_py/besttime_to_buyandsell_121.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buyprices = prices[0]
+        profit = 0
+        for i in range(0,len(prices)):
+            if(buyprices > prices[i]):
+                buyprices = prices[i]
+            profit = max(profit,prices[i] - buyprices)
+        return profit
