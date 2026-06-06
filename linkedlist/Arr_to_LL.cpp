5a2fda36052ab133104ea1439bfe6f2cc355e40d
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-06-06 23:20:06 +0530

    Implement delete and print functions for linked list
    Added deletion function for linked list and print function.

--- linkedlist/Arr_to_LL.cpp
+++ linkedlist/Arr_to_LL.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 class Node{
 public:
-    int data;
-    Node*next;
-    Node(int val){
-        data = val;
-        next = nullptr;
-    }
+int data;
+Node*next;
+Node(int value){
+    data = value;
+    next = nullptr;
+}
 };
-Node* convert(int arr[],int n){
-    Node*head= new Node(arr[0]);
+// creation of Linked list
+Node*convert(int arr[],int n){
+    Node*head = new Node(arr[0]);
     Node*mover = head;
     for(int i=1;i<n;i++){
         Node*temp = new Node(arr[i]);
@@ -19,15 +19,54 @@ Node* convert(int arr[],int n){
         mover = temp;
     }
     return head;
+}//conversion of array into LL
+// deletion of element from the LL
+Node*deltarget(int target,Node*head){
+    Node*temp = head;//creating copy of the head
+    if(head ==nullptr){
+        return nullptr;
+    }
+    if(head->data == target){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    while(temp->next != nullptr){
+        if(temp->next->data == target){
+            Node*value = temp->next;
+            temp->next = temp->next->next;
+            delete value;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
 }
+// print function
+void print(Node*head){
+    Node*temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"null"<<endl;
+}
+//main function 
 int main(){
-    int nums[]={0,1,2,3,4,5,6};
-    int n = sizeof(nums);
-    Node* LL= convert(nums,n);
-    Node* temp = LL;
 
-while(temp != nullptr){
-    cout << temp->data << "->";
-    temp = temp->next;
-}
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    Node* head = convert(arr,n);
+
+    cout << "Original Linked List: "<<endl;
+    print(head);
+
+    head = deltarget(30, head);
+
+    cout << "After deleting 30: ";
+    print(head);
+
+    return 0;
 }
