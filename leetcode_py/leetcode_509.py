e180e9215f2b15100883dff42de7252fef456512
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-06-11 19:36:25 +0530

    Create leetcode_509.py

---
+++ leetcode_py/leetcode_509.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if(n==0):
+            return 0
+        if(n==1):
+            return 1
+        a=0
+        b=1
+        for i in range (1,n):
+            next = a+b
+            a=b
+            b=next
+        return b
+        
