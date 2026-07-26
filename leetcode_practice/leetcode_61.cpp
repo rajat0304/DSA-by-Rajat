2c5b8afa4241941fcaf8a31f47e1d1811d0e479e
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-07-26 18:53:22 +0530

    Create leetcode_61.cpp

---
+++ leetcode_practice/leetcode_61.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL||k==0){
+            return head;
+        }
+        ListNode*last=head;
+        int len=1;
+        while(last->next != NULL){
+            last=last->next;
+            len++;
+        }
+        k=k%len;
+        if(k==0){
+            return head;
+        }
+        last->next=head;
+        ListNode*newtail=head;
+        for(int i=0;i<len-k-1;i++){
+            newtail=newtail->next;
+        }
+        ListNode*newhead=newtail->next;
+        newtail->next=NULL;
+        return newhead;
+    }
+};
