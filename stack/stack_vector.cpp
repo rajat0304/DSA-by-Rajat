8e1463ffebb319869b78852f56df5ae3970a6bfd
Rajat Tripathi <rajattripathi0304@gmail.com>
2026-08-21 19:31:12 +0530

    Add files via upload

---
+++ stack/stack_vector.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class stack{
+    vector<int>v;
+    public:
+        void push(int value){
+            v.push_back(value);
+        }
+        void pop(){
+            v.pop_back();
+        }
+        int top(){
+            return v[v.size()-1];
+        }
+        bool empty(){
+            return v.size() == 0;
+        }
+};
+int main(){
+    stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    while( !s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+    return 0;
+}
